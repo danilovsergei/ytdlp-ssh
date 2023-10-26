f87d19f35f1a4027980042346dcdde0c912eba03
sergeidanilov <sergeidanilov@gmail.com>
2023-10-26 15:25:58 -0700

    optimized build to shrink the final binary from 11MB to 2.6MB using ldflags during build and upx on the final binary

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ bin=$(echo $project_dir"/bin/ytdlp-ssh")
 timestamp=$(date -d "@$(date +%s)" +"%y-%m-%d")
 cd "$project_dir/cmd/ytdlp-ssh"
 
-go build -o $bin/ytdlp-ssh
+go build -ldflags="-s -w" -o $bin/ytdlp-ssh
 cp -r $presets_dir $bin"/"
 
 cd  $project_dir"/bin"
+upx ytdlp-ssh/ytdlp-ssh
 zip -r $timestamp-release.zip ytdlp-ssh 
