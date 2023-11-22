8620027d5570fe16dac59953a3907d45cbf0295d
sergeidanilov <sergeidanilov@gmail.com>
2023-11-21 21:46:54 -0800

    fixed downloading when destination folder contains single quote

--- sshclient/common.go
+++ sshclient/common.go
@@ -9,7 +9,8 @@ import (
 func (c *Connection) IsFileOrDirExists(dir string) bool {
 	retryFunc := func(output string) bool { return output == "" }
 
-	out := c.ExecCommandWithOutput(fmt.Sprintf("[ -d '%s' ] && echo true || echo false", dir), retryFunc)
+	out := c.ExecCommandWithOutput(fmt.Sprintf("[ -d \"%s\" ] && echo true || echo false", dir), retryFunc)
+
 	result, err := strconv.ParseBool(out)
 	if err != nil {
 		log.Fatalf("Failed to parse ssh out %s %s", out, err)
